14aba3263f2ceabf2142600c7908c268045fbbb1
Evelynn Kaplan <evelynnkaplan@gmail.com>
2019-06-10 15:19:10 -0700

    refactored timeline event code to implement CSS classnames in scaffolding

--- src/components/TimelineEvent.js
+++ src/components/TimelineEvent.js
@@ -4,10 +4,10 @@ import Timestamp from './Timestamp';
 
 const TimelineEvent = (props) => {
   return (
-    <section>
-      <h3>{props.person}</h3>
-      <p>{props.status}</p>
-      <p><Timestamp time={props.timestamp}/></p>
+    <section className="timeline-event">
+      <p className="event-person">{props.person}</p>
+      <p className="event-status">{props.status}</p>
+      <p className="event-time"><Timestamp time={props.timestamp}/></p>
     </section>
   );
 }
