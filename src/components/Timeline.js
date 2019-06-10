7d0b42c0e028631b2bdb446b8b35b08cbb81817e
Evelynn Kaplan <evelynnkaplan@gmail.com>
2019-06-10 16:20:06 -0700

    fixed capitalization bug in timestamp

--- src/components/Timeline.js
+++ src/components/Timeline.js
@@ -2,17 +2,15 @@ import React from 'react';
 import './Timeline.css';
 import TimelineEvent from './TimelineEvent';
 
-const Timeline = (eventsList) => {
+const Timeline = (props) => {
   const timelineEvents = [];
 
-  console.log(eventsList);
-
-   eventsList.events.forEach((event, i) => {
+   props.events.forEach((event, i) => {
     timelineEvents.push(<TimelineEvent 
     key={i} 
     person={event.person}
     status={event.status}
-    timestamp={event.timestamp} 
+    timeStamp={event.timeStamp} 
     />);
     })
 
--- src/components/TimelineEvent.js
+++ src/components/TimelineEvent.js
@@ -3,11 +3,12 @@ import './TimelineEvent.css';
 import Timestamp from './Timestamp';
 
 const TimelineEvent = (props) => {
+  console.log(props.timeStamp);
   return (
     <section className="timeline-event">
       <p className="event-person">{props.person}</p>
       <p className="event-status">{props.status}</p>
-      <p className="event-time"><Timestamp time={props.timestamp}/></p>
+      <p className="event-time"><Timestamp time={props.timeStamp}/></p>
     </section>
   );
 }
