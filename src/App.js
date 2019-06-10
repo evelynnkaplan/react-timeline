beb560e0cc176afcdd5b54f1323a1521e35cdd7d
Evelynn Kaplan <evelynnkaplan@gmail.com>
2019-06-10 15:15:10 -0700

    wrote Timeline Event and tested in app

--- src/App.js
+++ src/App.js
@@ -3,18 +3,25 @@ import './App.css';
 import timelineData from './data/timeline.json';
 
 import Timeline from './components/Timeline';
+import TimelineEvent from './components/TimelineEvent';
 
 class App extends Component {
   render() {
     console.log(timelineData);
 
-    // Customize the code below
+    const event = {
+      person: "Person 1",
+      status: "Hangin' out",
+      timestamp: "2018-05-18T22:12:03Z",
+    }
+
     return (
       <div className="App">
         <header className="App-header">
           <h1 className="App-title">Application title</h1>
         </header>
         <main className="App-main">
+          <TimelineEvent person={event.person} status={event.status} timestamp={event.timestamp} />
         </main>
       </div>
     );
--- src/components/TimelineEvent.js
+++ src/components/TimelineEvent.js
@@ -2,9 +2,14 @@ import React from 'react';
 import './TimelineEvent.css';
 import Timestamp from './Timestamp';
 
-const TimelineEvent = () => {
-  // Fill in your code here
-  return;
+const TimelineEvent = (props) => {
+  return (
+    <section>
+      <h3>{props.person}</h3>
+      <p>{props.status}</p>
+      <p><Timestamp time={props.timestamp}/></p>
+    </section>
+  );
 }
 
 export default TimelineEvent;
